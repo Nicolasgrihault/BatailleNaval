9532dacef591f07bc5520f7d653a434b43cd789e
NicolasG <nicolas.grihault@imie.fr>
2018-10-18 12:10:07 +0200

    start

---
+++ src/main/java/fr/imie/BatailleNaval/ApplicationBatailleNaval.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.imie.BatailleNaval;
+
+/**
+ * @author NicolasG
+ *
+ */
+public class ApplicationBatailleNaval {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
